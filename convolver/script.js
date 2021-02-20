3156178134aed40a8e47b4dfe95a942608dd9755
Rumyra <rumyra@gmail.com>
2021-02-20 21:14:48 +0000

    changing to use mp3s

--- convolver/script.js
+++ convolver/script.js
@@ -1,5 +1,3 @@
-import irFiles from './encoded.js'
-
 console.clear();
 
 console.log('%cHiya! 👋 Thanks for taking a look at my codes 😎 Follow me on twitter or instagram to see more of my work; I\'m @Rumyra 🐧', 'background-color: hsla(315, 3%, 25%, 1); font-size: 120%; line-height: 1.4; text-align: center; color: hsla(33, 55%, 92%, 1); border: 2px solid hsla(273, 36%, 64%, 1); padding: 0.5em;');
@@ -7,7 +5,6 @@ console.log('%cHiya! 👋 Thanks for taking a look at my codes 😎 Follow me on
 // create an audio element to add the reverb to
 const audioEl = new Audio('110_E_Arps_SP_01.mp3');
 audioEl.crossOrigin = 'anonymous';
-
 
 // instigate audio context
 const actx = new AudioContext();
@@ -17,14 +14,41 @@ const source = actx.createMediaElementSource(audioEl);
 const reverbNode = new ConvolverNode(actx);
 
 // select the correct file
-// data-file attr matches imported irFiles
+// data-file attr matches imported mp3Files
 let useFile = 'orig';
 source.connect(actx.destination);
+
+// load file function for samples
+// returns buffer
+async function getFile(audioContext, filepath) {
+  const response = await fetch(filepath);
+  const arrayBuffer = await response.arrayBuffer();
+  const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
+  return audioBuffer;
+}
 
 // quick & dirty: grab our buttons
 const buttons = document.querySelectorAll('.select--btns button');
 
-for (const butt of buttons) {
+const samples = [];
+// initiate by downloading samples
+function init() {
+  for (const butt of buttons) {
+
+    if (butt.dataset.file === 'orig') {
+      samples.push(null);
+    } else {
+      getFile(actx, 'mp3/'+butt.dataset.file+'.mp3').then( (sample) => {
+        samples.push(sample);
+        butt.disabled = false;
+      });
+    }
+  }  
+}
+init();
+
+// connect the graph on click
+buttons.forEach( (butt, i) => {
   butt.addEventListener('click', () => {
     
     // change the current button state
@@ -34,54 +58,16 @@ for (const butt of buttons) {
     // switch to our new button
     butt.dataset.selected = 'true';
     
-    // update the graphs
-    console.log(butt.dataset.file);
-    useFile = butt.dataset.file;
-
     // update audio graph
-    connectGraph(useFile);
+    if (butt.dataset.file === 'orig') {
+      reverbNode.disconnect();
+    } else {
+      reverbNode.buffer = samples[i];
+      // connect our graph
+      source.connect(reverbNode).connect(actx.destination);
+    }
   })
-}
-
-
-
-// using a base64 encoded file as a convolver inspired by this pen https://codepen.io/DonKarlssonSan/pen/doVKRE?editors=0010
-// function to return a buffer from the base64
-function base64ToArrayBuffer(base64) {
-  var binaryString = window.atob(base64);
-  var len = binaryString.length;
-  var bytes = new Uint8Array(len);
-  for (var i = 0; i < len; i++)        {
-      bytes[i] = binaryString.charCodeAt(i);
-  }
-  return bytes.buffer;
-}
-
-// YOU ARE HERE create a function to switch node buffer
-
-function connectGraph(fileRef) {
-  if (fileRef === 'orig') {
-    reverbNode.disconnect();
-    // source.connect(actx.destination);
-  } else {
-    // Convert it to a binary array first
-    var reverbSoundArrayBuffer = base64ToArrayBuffer(irFiles[fileRef]);
-
-    actx.decodeAudioData(reverbSoundArrayBuffer, 
-      function(buffer) {
-        reverbNode.buffer = buffer;
-      },
-      function(e) {
-        alert("Error when decoding audio data" + e.err);
-      }
-    );
-
-    // connect our graph
-    source.connect(reverbNode).connect(actx.destination);
-  }
-}
-
-
+} )
 
 // play/pause functionality
 const playButton = document.querySelector('#play-button');
